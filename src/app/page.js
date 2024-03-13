b43519da6258ebea29a269e26b20cb63ec0d2a73
Ale11057 <marcogarr84@gmail.com>
2024-03-12 21:11:51 -0600

    changes 1.0

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 "use client";
-import Image from "next/image";
 import Splash from "./components/splash.js";
-import { Container } from "postcss";
 export default function Home() {
   return (
     <>
